4d9082cbffde190d1c1da06f7ce2c0312598cf38
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-21 11:24:50 -0700

    add register for user

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/user.service';
 import { ClientMessage } from './../../model/client-message';
 import { User } from './../../model/user';
 import { Component, OnInit } from '@angular/core';
@@ -6,16 +7,22 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public user = new User(0, '', '', '', '', '', []);
+  public clientMessage = new ClientMessage('');
 
-  public user = new User(0,'','','','','',[]);
-  public ClientMessage = new ClientMessage('');
+  constructor(private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public addUser(): void {
+    this.userService.addUser(this.user).subscribe(
+      (data) =>
+        (this.clientMessage.message = `Successfully added ${data.firstName}`),
+      (error) => (this.clientMessage.message = `Error was ${error}`)
+    );
+    console.log(this.user);
   }
-
 }
