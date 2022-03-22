ee2c04e307bc6c6667f02bf3d773e195764db968
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-22 10:40:44 -0700

    add findListDetails to component

--- src/app/components/list-details/list-details.component.ts
+++ src/app/components/list-details/list-details.component.ts
@@ -1,3 +1,4 @@
+import { ListService } from './../../services/list.service';
 import { ClientMessage } from './../../model/client-message';
 import { Item } from './../../model/Item';
 import { ToDoList } from './../../model/ToDoList';
@@ -13,23 +14,28 @@ import { ActivatedRoute } from '@angular/router';
 export class ListDetailsComponent implements OnInit {
 
   public toDoList = new ToDoList(0,'','',false,[]);
-  public item: Item[] = [];
+  public itemList: Item[] = [];
 
   public ClientMessage: ClientMessage = new ClientMessage(
     "No items in this list yet"
   );
 
   constructor(private userServ: UserService,
+    private listServ: ListService,
     private route: ActivatedRoute
     ) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const listIdFromRoute = Number(routeParams.get('listId'));
+    this.findListDetails(listIdFromRoute);
 
   }
 // find the list details from the list id number extracted from the route
   findListDetails(id:number) {
+    this.listServ.getItemListByListId(id).subscribe((data) => {
+      this.itemList = data;
+    })
 
   }
 
